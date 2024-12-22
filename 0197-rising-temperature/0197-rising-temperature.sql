1f053a70db547d8a8cf8f761d947216f163f7d84
JunHyeok, Kim <120662329+JunHyeokDev@users.noreply.github.com>
2024-12-22 15:14:33 +0100

    Time: 851 ms (16.54%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,14 @@
 /* Write your T-SQL query statement below */
 
-select w2.id
-from Weather as w1
-join Weather as w2
-on w1.recordDate = DATEADD(day,-1,w2.recordDate)
-where w1.temperature < w2.temperature
+
+-- select today.id
+-- from Weather as yesterday
+-- cross join Weather as today
+
+-- where DateDiff(day,today.recordDate,yesterday.recordDate) = 1
+-- and today.temperature > yesterday.temperature
+
+SELECT today.id
+FROM Weather AS yesterday
+JOIN Weather AS today ON DATEADD(day, 1, yesterday.recordDate) = today.recordDate
+WHERE today.temperature > yesterday.temperature;
